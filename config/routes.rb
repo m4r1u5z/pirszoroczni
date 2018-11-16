87b00dadaab18df330acfed14e520d32e3ccd174
m4r1u5z <pekeyprv@gmail.com>
2018-11-16 21:13:28 +0100

    Generate pages index contact about

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'pages#index'
+  # get 'hello', to: 'pages#contact'
+  get 'pages/contact'
+  get 'pages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
